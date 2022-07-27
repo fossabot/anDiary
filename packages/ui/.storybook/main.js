45bfc39d3f639913fb003a3b666aa5306cdd5889
Naffy Dharni <naffydharni006@gmail.com>
2022-07-27 18:53:30 +0530

    chore: enhancement?

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -3,7 +3,6 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-postcss',
     {
       name: '@storybook/addon-postcss',
       options: {
