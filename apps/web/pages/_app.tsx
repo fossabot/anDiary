d2685a7683f2780ca32fff6f2371978b37456180
Naffy Dharni <naffydharni006@gmail.com>
2022-07-31 19:53:41 +0530

    fix(web): styles are now enabled for ui library classes

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-// import "andiary-ui/src/components/Button/Button.css"
+import "../styles/global.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.tsx",
+    "./node_modules/ui/src/**/*.tsx"
+  ],
   theme: {
     extend: {},
   },
