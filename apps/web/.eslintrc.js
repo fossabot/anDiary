53479c7f43eb87df9424fcb2901abd150e0fcc14
Naffy Dharni <naffydharni006@gmail.com>
2022-07-31 16:04:33 +0530

    chore: configure eslint

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
-  extends: ["custom"],
+  extends: ["custom", "next", "next/core-web-vitals"],
 };
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "next/babel","next/core-web-vitals", "next", "prettier"],
+  extends: ["eslint:recommended", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
