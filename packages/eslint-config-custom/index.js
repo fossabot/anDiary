e119605033b927cd220dff9f5bdeab458b1a09e2
Naffy Dharni <naffydharni006@gmail.com>
2022-08-04 19:48:07 +0530

    chore: remove eslint shared config

--- packages/eslint-config-custom/index.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-	extends: ['eslint:recommended', 'prettier'],
-	rules: {
-		'@next/next/no-html-link-for-pages': 'off',
-		'react/jsx-key': 'off'
-	}
-}
