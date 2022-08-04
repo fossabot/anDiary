84c391487c5e838742bef081cf8f9355723276b9
Naffy Dharni <naffydharni006@gmail.com>
2022-08-04 19:45:23 +0530

    chore: remove site

--- apps/site/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./pages/**/*.tsx', './node_modules/ui/src/**/*.tsx', './components/**/*.tsx'],
-	theme: {
-		extend: {}
-	},
-	plugins: [require('daisyui')]
-}
