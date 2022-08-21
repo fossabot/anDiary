cb5c67307d3dd831621e8181072d65f61cd21a84
Naffy Dharni <naffydharni006@gmail.com>
2022-08-21 12:03:57 +0000

    docs: update homepage

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,39 +1,36 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-
+import React from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx()}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className="">
-          <Link
-            className="btn btn-primary"
-            to="/download">
-            Download Now
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext()
+	return (
+		<>
+			<div className="hero min-h-screen bg-base-200">
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<h1 className="text-5xl font-bold">{siteConfig.title}</h1>
+						<p className="py-6">
+							{siteConfig.tagline}
+						</p>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description={siteConfig.tagline}>
-      <HomepageHeader />
-      <main>
-        Hello people 👋
-      </main>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext()
+	return (
+		<Layout
+			title={`Hello from ${siteConfig.title}`}
+			description={siteConfig.tagline}>
+			<HomepageHeader />
+			<main>Hello people 👋</main>
+		</Layout>
+	)
 }
